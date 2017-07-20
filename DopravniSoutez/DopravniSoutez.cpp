e6a9f4a2b519c4bd26a0d6259d968c8bf681499c
Ondřej Horka <horka.ondrej@gmail.com>
2017-07-20 18:08:38 +0200

    Ubuntu gtkmm working

--- DopravniSoutez/DopravniSoutez.cpp
+++ DopravniSoutez/DopravniSoutez.cpp
@@ -6,21 +6,30 @@ using namespace std;
 
 int main(int argc, char* argv[]) {
 
-	Database *db;
-	db = new Database("drcomp");
+	auto app =
+    Gtk::Application::create(argc, argv,
+      "org.gtkmm.examples.base");
+
+  	Gtk::Window window;
+  	window.set_default_size(200, 200);
+
+  	return app->run(window);
+
+	// Database *db;
+	// db = new Database("drcomp");
 	//db->query("CREATE TABLE a (a INTEGER, b INTEGER);");
 	//db->query("INSERT INTO a VALUES(1, 2);");
 	//db->query("INSERT INTO a VALUES(5, 4);");
 
-	vector<vector<string> > result = db->query("SELECT a, b FROM a;");
+	// vector<vector<string> > result = db->query("SELECT a, b FROM a;");
 
-	for (vector<vector<string> >::iterator it = result.begin(); it < result.end(); ++it)
-	{
-		vector<string> row = *it;
-		cout << row.at(0) << row.at(1) << row.at(2) << row.at(3) << row.at(4) << row.at(5) << row.at(6) << row.at(7) << row.at(8) << row.at(9) << endl;
-	}
-	cin.get();
+	// for (vector<vector<string> >::iterator it = result.begin(); it < result.end(); ++it)
+	// {
+	// 	vector<string> row = *it;
+	// 	cout << row.at(0) << row.at(1) << row.at(2) << row.at(3) << row.at(4) << row.at(5) << row.at(6) << row.at(7) << row.at(8) << row.at(9) << endl;
+	// }
+	// cin.get();
 
-	db->close();
+	// db->close();
 }
 
